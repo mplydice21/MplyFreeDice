a85afecd99923112c82b2be90f8fd8dc46d0ef4b
mplydice21 <mplyfreedice+gktkt@gmail.com>
2025-06-12 09:49:33 +0530

    Update basic.cy.js

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -3,9 +3,11 @@ describe('empty spec', () => {
     cy.visit('/')
   })
   it('displays the resources text', () => {
-    cy.get('h2')
-    .contains('This is a bare-bones Hugo project that has everything you need to quickly deploy it to Netlify');
-  })
+  cy.get('h2').then(($h2) => {
+    console.log($h2.text()); // This will log the text content of the h2 to the console
+    expect($h2.text()).to.include('This is a bare-bones Hugo project');
+  });
+});
   it('renders the image', () => {
     cy.get('img')
     .should('be.visible')
